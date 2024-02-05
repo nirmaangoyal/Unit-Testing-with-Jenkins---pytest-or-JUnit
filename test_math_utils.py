e4f7f005cc9c796d6b2f7b2e1b606acda4214b13
nirmaan goyal <nirmaan.goyal.23@gmail.com>
2024-02-04 18:01:35 -0800

    jenkins 2nd commit

--- test_math_utils.py
+++ test_math_utils.py
@@ -7,17 +7,30 @@ def math_utils():
 
 def test_add(math_utils):
     assert math_utils.add(2, 3) == 5
-    # Add more test cases
+    assert math_utils.add(-1, 1) == 0 
+    assert math_utils.add(-1, -1) == -2  
+    assert math_utils.add(0, 0) == 0  
 
 def test_subtract(math_utils):
     assert math_utils.subtract(3, 2) == 1
-    # Add more test cases
+    assert math_utils.subtract(2, 3) == -1  
+    assert math_utils.subtract(-1, -1) == 0  
+    assert math_utils.subtract(0, 0) == 0  
 
 def test_multiply(math_utils):
     assert math_utils.multiply(2, 3) == 6
-    # Add more test cases
+    assert math_utils.multiply(-1, 1) == -1  
+    assert math_utils.multiply(-1, -1) == 1  
+    assert math_utils.multiply(0, 5) == 0  
+    assert math_utils.multiply(0, 0) == 0  
+
 
 def test_divide(math_utils):
     assert math_utils.divide(4, 2) == 2.0
-    assert math_utils.divide(4, 0) == -1.0  # Test division by zero
-    # Add more test cases
+    assert math_utils.divide(4, 0) == -1.0  
+    assert math_utils.divide(0, 4) == 0.0  
+    assert math_utils.divide(5, 2) == 2.5  
+    assert math_utils.divide(-4, 2) == -2.0  
+    assert math_utils.divide(-4, -2) == 2.0  
+    assert math_utils.divide(4, -2) == -2.0  
+
